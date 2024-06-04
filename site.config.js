fdb811fbf6d2546f389b46836c45245bd092513e
Limchaeyoung <102286483+chaeyoungeee@users.noreply.github.com>
2024-06-04 15:44:04 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Limchaeyoung",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "backend developer",
+    bio: "",
+    email: "limcy423@gmail.com",
+    // linkedin: "morethanmin",
+    github: "chaeyoungeee",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `limchaeyoung`,
+      href: "https://github.com/chaeyoungeee",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "chaeyoung-log",
+    description: "welcome to chaeyoung-log!",
   },
 
   // CONFIG configration (required)
